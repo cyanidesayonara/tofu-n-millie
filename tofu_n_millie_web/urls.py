a78857c7ad34aed0c37c02206bf3ca4ec14cb841
Santtu Nykänen <santtu.nykanen@gofore.com>
2026-03-09 15:42:46 +0100

    Fix media serving in production and add SEO meta tags

--- tofu_n_millie_web/urls.py
+++ tofu_n_millie_web/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gallery.urls')),
+    # Always serve media files (fine for a small site; use S3 or similar for production)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-# Always serve media files (fine for a prototype; use S3 or similar for production)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
